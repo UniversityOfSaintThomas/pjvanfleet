6c15724eba852ae8f681b61380a7f1d50523eceb
Vue <vue07418@STTHOMAS.EDU>
2023-05-19 10:44:24 -0500

    add file .zip and .MOV

---
+++ wavelets/examples/Chapter4/mfiles/Example4.10.m
@@ -0,0 +1,13 @@
+%% Title
+%
+% In this lab, we will learn how to perform some basic image processing
+% functions.
+%
+%% Reference
+%
+% The material for this lab is covered in Section X.X of the book ...
+%
+%
+%% WaveletWare Commands Used in this Notebook
+%
+%%
